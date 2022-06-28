b1cc57337a23e8b9aa38b93f3827d820cc1d84c3
Mrain00 <luxassilva@gmail.com>
2022-06-28 17:14:52 -0300

    permute

---
+++ permute.js
@@ -0,0 +1,19 @@
+let permute = function(nums) {
+    let tempa=[];
+    let final=[];
+    function backtrack(tempa,nums){
+        if(nums.length===0){
+            final.push(tempa.slice());
+            return;
+        }
+        for(let i=0;i<nums.length;i++){
+            tempa.push(nums[i]);
+            nums.splice(i,1);
+            backtrack(tempa,nums,final);
+            nums.splice(i,0,tempa.pop());
+
+        }
+    }
+    backtrack(tempa,nums,final);
+    return final;
+}
